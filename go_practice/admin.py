c8e19acbed401a7a1c98bcf54273449c3153846f
Camila Maia <cmaiacd@gmail.com>
2020-04-02 19:21:17 -0300

    Add go_practice app with Exercise model

--- go_practice/admin.py
+++ go_practice/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from go_practice.models import Exercise
 
-# Register your models here.
+
+class ExerciseAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Exercise, ExerciseAdmin)
--- go_practice/models.py
+++ go_practice/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Exercise(models.Model):
+    TYPES = (
+        ("scale", "Scale"),
+        ("arpeggio", "Arpeggio"),
+        ("chord", "Chord"),
+    )
+    NOTES = (
+        ("C", "C"),
+        ("C", "C#"),
+        ("D", "D"),
+        ("D#", "D#"),
+        ("E", "E"),
+        ("F", "F"),
+        ("F#", "F#"),
+        ("G", "G"),
+        ("G#", "G#"),
+        ("A", "A"),
+        ("A#", "A#"),
+        ("B", "B"),
+    )
+
+    QUALITIES = (
+        ("major", "Major Triad"),
+        ("minor", "Minor Triad"),
+        ("diminished_triad", "Diminished Triad"),
+        ("major7", "Major 7th"),
+        ("dominant", "Dominant 7th"),
+        ("minor7", "Minor 7th"),
+        ("half-diminished", "Half-diminished 7th"),
+        ("diminished7", "Diminished 7th"),
+    )
+
+    type = models.CharField(max_length=25, choices=TYPES)
+    note = models.CharField(max_length=2, choices=NOTES)
+    quality = models.CharField(max_length=25, choices=QUALITIES)
+
+    class Meta:
+        unique_together = ("type", "note", "quality")
+
+    def __str__(self):
+        return f"{self.type} - {self.note} - {self.quality}"
